1ddf702b8b33d175f59b33cbeb25edf19393e588
Guatemala <Guatemala@Guatemala-PC>
2017-08-09 20:11:47 -0600

    Tarea 3 finalizada con Éxito

--- src/tarea_3/Tarea_3.java
+++ src/tarea_3/Tarea_3.java
@@ -4,127 +4,147 @@
  * and open the template in the editor.
  */
 package tarea_3;
+
 import java.util.*;
 
 
 public class Tarea_3 {
- 
+
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String VectorUsuario[] = new String[5];
-        VectorUsuario[0]= "nombre1";
-        VectorUsuario[1]= "nombre2";
-        VectorUsuario[2]= "nombre3";
-        VectorUsuario[3]= "nombre4";
-        VectorUsuario[4]= "nombre5";
-        String nombre=" ";
-        String buscar="";
-        int opcion=0;
-       
-        boolean cerrar=true;
-        
-        
-        System.out.println("Tarea 3"); 
-        System.out.println("201602470"); 
-        System.out.println("1. Usuarios"); 
-        System.out.println("2. Palabras Palindromas"); 
-        System.out.println("3. Salir");
-        
-        while(cerrar){
-        opcion=sc.nextInt();
-        switch(opcion){
-            case 1:
-                
-                switch (opcion){
-                    case 1:
-                        System.out.println("Menu de Usuarios"); 
-                        System.out.println("1. Ingresar Usuarios");
-                        System.out.println("2. Mostrar todos los Usuarios"); 
-                        System.out.println("3. Mostrar un Usuario Personalizado");
-                        System.out.println("4. Menú Principal"); 
-                        System.out.println("5. Salir");
-                        
-                        
-                        opcion=sc.nextInt();
-                            switch(opcion){
-                                case 1:
-                        
-                       for (int i=0; i <VectorUsuario.length; i++)
-                       {
-                           System.out.println("Ingresar Usuarios");
-                           nombre=sc.next();
-                           VectorUsuario[i]= nombre;
-                           
-	} 
-        break;
-        
-        
-                                case 2:
-                                    System.out.println("Mostrar todos los Usuarios");
-                                    System.out.println("1. "+VectorUsuario[0]+"\n2. "+VectorUsuario[1]+"\n3. "+VectorUsuario[2]+"\n4. "+VectorUsuario[3]+"\n5. "+VectorUsuario[4]);
-                               break;
-                                case 3:
-                                    System.out.println("Mostrar un Usuario Personalizado");
-                                    System.out.println("Ingresar Usuario ");
-                                    for (int i=0; i <VectorUsuario.length; i++){
-                                       if (buscar == nombre){
-                                           buscar=sc.next();
-                                        System.out.println(buscar);
+
+        Scanner sc = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
+        String[] VectorUsuario = new String[5];
+        String nombre;
+        String buscar;
+        int opcion = 0;
+        boolean cerrar = true;
+        boolean existe = false;
+        System.out.println("        *****  TAREA 3  *****       ");
+        System.out.println("\n              201602470           ");
+        System.out.println("\n            1. Usuarios");
+        System.out.println("            2. Palabras Palindromas");
+        System.out.println("            3. Salir");
+
+        while (cerrar) {
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+
+                    switch (opcion) {
+                        case 1:
+                            boolean close = true;
+                            while (close) {
+                                System.out.println("     *****  MENÚ DE USUARIOS  *****");
+                                System.out.println("\n            1. Ingresar Usuarios");
+                                System.out.println("            2. Mostrar todos los Usuarios");
+                                System.out.println("            3. Mostrar un Usuario Personalizado");
+                                System.out.println("            4. Menú Principal");
+                                System.out.println("            5. Salir");
+
+                                opcion = sc.nextInt();
+                                switch (opcion) {
+                                    case 1:
+
+                                        for (int i = 0; i < VectorUsuario.length; i++) {
+                                            System.out.println("            Ingresar Usuarios           ");
+                                            nombre = teclado.nextLine();
+                                            VectorUsuario[i] = nombre;
+
+                                        }
+                                        System.out.println("            \n           ");
+                                        break;
+
+                                    case 2:
+                                        System.out.println("            \n           ");
+                                        for (int i = 0; i < VectorUsuario.length; i++) {
+                                            System.out.println(i + 1 + ". " + VectorUsuario[i]);
+                                        }
+                                        System.out.println("            \n           ");
+                                        break;
+
+                                    case 3:
+                                        System.out.println("        Mostrar un Usuario Personalizado");
+                                        System.out.println("\n            Ingresar Usuario: ");
+                                        buscar = teclado.nextLine();
+
+                                        for (int i = 0; i < VectorUsuario.length; i++) {
+
+                                            if (buscar.equalsIgnoreCase(VectorUsuario[i])) {
+                                                System.out.println("\n        Usuario \n\n " + VectorUsuario[i]);
+                                                existe = true;
+                                            }
+
+                                        }
+
+                                        if (existe == false) {
+                                            System.out.println(" \n\nERROR! No existe ningún usuario con esa coincidencia");
+                                        }
+                                        System.out.println("            \n           ");
+                                        break;
+
+                                    case 4:
+                                        boolean op2 = true;
+                                        while (op2) {
+                                            System.out.println("            \n           ");
+                                            System.out.println("        *****  TAREA 3  *****       ");
+                                            System.out.println("\n              201602470           ");
+                                            System.out.println("\n            1. Usuarios");
+                                            System.out.println("            2. Palabras Palindromas");
+                                            System.out.println("            3. Salir");
+                                            break;
+                                        }
+                                        op2 = false;
+
+                                    case 5:
+
+                                        close = false;
+                                       
+                                }
+
+                            }
+                            break;
+                    }
+                    break;
+
+                case 2:
+
+                    Scanner leer = new Scanner(System.in);
+                    String palabra = "";
+                    String palindromo = "";
+                    System.out.println("            \n           ");
+                    System.out.println("        *****  PALABRAS PALINDROMAS  *****");
+                    System.out.println("\n               Ingresar Palabra");
+                    palabra = leer.nextLine();
+
+                    for (int i = palabra.length() - 1; i >= 0; i--) {
+                        palindromo += palabra.charAt(i);
+                    }
+
+                    if (palabra.equalsIgnoreCase(palindromo)) {
+                        System.out.println("\n        La palabra SI ES PALINDROMA!!!");
+                    } else {
+                        System.out.println("\n        La palabra NO ES PALINDROMA!!!");
+                    }
+                    boolean op2 = true;
+                    while (op2) {
+                        System.out.println("            \n           ");
+                        System.out.println("        *****  TAREA 3  *****       ");
+                        System.out.println("\n              201602470           ");
+                        System.out.println("\n            1. Usuarios");
+                        System.out.println("            2. Palabras Palindromas");
+                        System.out.println("            3. Salir");
+                        break;
+                    }
+                    op2 = false;
+
+                    break;
+
+                case 3:
+                    cerrar = false;
+                    break;
+            }
+
         }
-                                       else {
-                                           buscar=sc.next();
-                                        System.out.println("ERROR! No existe ningun usuario con esa coincidencia");
-        } break; 
-                                    }
-                        
-                            }break;
-                }
-       break;
-       
-            case 2:
-               /* Scanner leer=new Scanner(System.in);
-                String palabra= leer.nextLine();
-                char[] caracteres= palabra.toCharArray();
-                char[] palindromo=new char[caracteres.length];
-                int num=0;
-                String palindromo2 = new String(palindromo);
-               
-               System.out.println("Ingrese palabra");
-                     palabra=leer.next();
-                 for(int i= caracteres.length; i>0; i--){
-                    
-                    palindromo[num]=caracteres[i-1];
-                    num++;
-                }
-                
-                if (palabra.equals(palindromo2)){
-                    System.out.println("Es un palindromo");
-                }
-                else{
-                    System.out.println("No es un palindromo");
-                }
-                break;*/
-                Scanner leer=new Scanner(System.in);
-                String palabra, palindromo="";
-                System.out.println("Palabras Palindromas");
-                System.out.println("Ingresar Palabra");
-                palabra=leer.nextLine();
-                
-                for(int x=palabra.length()-1; x>= 0; x--)
-                    palindromo+=palabra.charAt(x);
-                if(palabra.equalsIgnoreCase(palindromo))
-                    System.out.println("La palabra es un palindromo");
-                else
-                    System.out.println("La palabra no es un palindromo");
-                break;
-        
-        case 3:
-            cerrar=false;
-            break;
-        }
-        
-        
-        }}}
-        
-          
+    }
+}
